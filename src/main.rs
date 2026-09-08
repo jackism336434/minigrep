8404b9fa36580d8b80a4a305cd8947f181263106
Aurelio <2531403873@qq.com>
2026-09-08 19:39:43 +0800

    feat:import Result and add read file feature"

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use std::{env, fs};
+use std::{env, error::Error, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem is:{err}");
+        process::exit(1);
+    });
+
+    run(config);
 }
 
 struct Config {
@@ -9,12 +14,17 @@ struct Config {
     file_path: String,
 }
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("No enough arguments");
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
+}
+
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.file_path).expect("No such file or directory");
+    println!("content:\n{contents}");
 }
