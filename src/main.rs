abb4b2be802d1f878f21acf91916b6c105687b25
Aurelio <2531403873@qq.com>
2026-09-08 19:13:56 +0800

    feat:add new error hint

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 }
 
 struct Config {
     query: String,
     file_path: String,
 }
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    Config { query, file_path }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("No enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
 }
